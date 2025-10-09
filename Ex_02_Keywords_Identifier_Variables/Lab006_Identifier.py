4649322e408300f65869b253a62aef37ab8a5dea
viswapython1 <viswapython1@gmail.com>
2025-10-09 08:33:42 +0530

    Python Variables identifier and key word Demonstration

--- Ex_02_Keywords_Identifier_Variables/Lab006_Identifier.py
+++ Ex_02_Keywords_Identifier_Variables/Lab006_Identifier.py
@@ -1,3 +1,11 @@
+"""1. Variable name.
+2. Rules for this.
+    1. _ Can be a variable.
+    2. Do not have a number as the first thing in a variable name.
+    3.  Alpha Numeric is possible as a variable name or identifier.
+
+"""
+
 age = 65
 _age = 65
 print(age)
