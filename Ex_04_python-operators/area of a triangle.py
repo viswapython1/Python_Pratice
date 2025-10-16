115d9d1a03237d3befe0de517c4767d1eb50db31
viswapython1 <viswapython1@gmail.com>
2025-10-16 09:45:39 +0530

    Task 14th Oct 2025 | Operator

---
+++ Ex_04_python-operators/area of a triangle.py
@@ -0,0 +1,9 @@
+#base = float(input("Enter base: "))
+#height = float(input("Enter height: "))
+#area = 0.5 * base * height
+#print("Area of triangle:", area)
+
+base = float(input("enter base: "))
+height = float(input("enter height: "))
+area= 0.5*base*height
+print("area of triangle:", area)
---
+++ Ex_04_python-operators/to check if a number is greater than 100.py
@@ -0,0 +1,18 @@
+# Program to check if a number is greater than 100
+
+# Take input from user
+"""num = float(input("Enter a number: "))
+
+# Check condition
+if num > 100:
+    print("The number is greater than 100.")
+else:
+    print("The number is not greater than 100.")"""
+
+num =float(input("enter a number: "))
+if num>100:
+    print("the number is greater than 100.")
+elif num == 100:
+    print("The number is equal to 100.")
+else:
+    print("the number is not greater than 100.")
